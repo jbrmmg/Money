ef7ba1c8a0ef59dcb2bd998c70006de2b8b07d07
jason <jason@jbrmmg.me.uk>
2022-10-12 17:13:28 +0100

    JBR-339 refactor money

--- src/main/java/com/jbr/middletier/money/control/AccountController.java
+++ src/main/java/com/jbr/middletier/money/control/AccountController.java
@@ -1,12 +1,10 @@
 package com.jbr.middletier.money.control;
 
-import com.jbr.middletier.money.data.Account;
-import com.jbr.middletier.money.dataaccess.AccountRepository;
 import com.jbr.middletier.money.dto.AccountDTO;
 import com.jbr.middletier.money.exceptions.AccountAlreadyExistsException;
 import com.jbr.middletier.money.exceptions.InvalidAccountIdException;
+import com.jbr.middletier.money.manager.AccountManager;
 import com.jbr.middletier.money.manager.LogoManager;
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by jason on 07/03/17.
@@ -30,19 +25,15 @@ import java.util.Optional;
 public class AccountController {
     private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);
 
-    private final AccountRepository accountRepository;
+
     private final LogoManager logoManager;
-    private final ModelMapper modelMapper;
+
+    private final AccountManager accountManager;
 
     @Autowired
-    public AccountController(AccountRepository accountRepository, LogoManager logoManager, ModelMapper modelMapper) {
-        this.accountRepository = accountRepository;
+    public AccountController(LogoManager logoManager, AccountManager accountManager) {
         this.logoManager = logoManager;
-        this.modelMapper = modelMapper;
-    }
-
-    public String getAccountLogo(String id, Boolean disabled) {
-        return this.logoManager.getSvgLogoForAccount(id,disabled).getSvgAsString();
+        this.accountManager = accountManager;
     }
 
     @GetMapping(path="/int/money/account/logo")
@@ -51,88 +42,45 @@ public class AccountController {
         LOG.info("Account Logo (int)");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.valueOf("image/svg+xml"));
-        return new ResponseEntity<>(getAccountLogo(id, disabled), headers, HttpStatus.OK);
+        return new ResponseEntity<>(this.logoManager.getSvgLogoForAccount(id,disabled).getSvgAsString(), headers, HttpStatus.OK);
     }
 
     @GetMapping(path="/ext/money/account/logo")
     public @ResponseBody ResponseEntity<String> getExtAccountLogo(@RequestParam(value="id", defaultValue="UNKN") String id,
                                                                   @RequestParam(value="disabled", defaultValue="false") Boolean disabled) {
         LOG.info("Account Logo (ext)");
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.valueOf("image/svg+xml"));
-        return new ResponseEntity<>(getAccountLogo(id, disabled), headers, HttpStatus.OK);
-    }
-
-    private List<AccountDTO> getAccounts() {
-        List<AccountDTO> result = new ArrayList<>();
-        for(Account nextAccount: accountRepository.findAll()) {
-            result.add(this.modelMapper.map(nextAccount, AccountDTO.class));
-        }
-
-        Collections.sort(result);
-
-        return result;
+        return getIntAccountLogo(id, disabled);
     }
 
     @GetMapping(path="/ext/money/accounts")
     public @ResponseBody List<AccountDTO> getExtAccounts() {
         LOG.info("Request Accounts (ext).");
 
-        return getAccounts();
+        return accountManager.getAccounts();
     }
 
     @GetMapping(path="/int/money/accounts")
     public @ResponseBody List<AccountDTO>  getIntAccounts() {
         LOG.info("Request Accounts (int).");
 
-        return getAccounts();
+        return accountManager.getAccounts();
     }
 
     @PostMapping(path="/int/money/accounts")
     public @ResponseBody List<AccountDTO> createAccount(@RequestBody AccountDTO account) throws AccountAlreadyExistsException {
         LOG.info("Create a new account - {}}", account.getId());
-
-        // Is there an account with this ID?
-        Optional<Account> existingAccount = accountRepository.findById(account.getId());
-        if(existingAccount.isPresent()) {
-            throw new AccountAlreadyExistsException(account);
-        }
-
-        accountRepository.save(this.modelMapper.map(account,Account.class));
-
-        return getAccounts();
+        return accountManager.createAccount(account);
     }
 
     @PutMapping(path="/int/money/accounts")
     public @ResponseBody List<AccountDTO> updateAccount(@RequestBody AccountDTO account) throws InvalidAccountIdException {
         LOG.info("Update an account - {}", account.getId());
-
-        // Is there an account with this ID?
-        Optional<Account> existingAccount = accountRepository.findById(account.getId());
-        if(existingAccount.isPresent()) {
-            existingAccount.get().setColour(account.getColour());
-            existingAccount.get().setImagePrefix(account.getImagePrefix());
-            existingAccount.get().setName(account.getName());
-            accountRepository.save(existingAccount.get());
-
-            return getAccounts();
-        }
-
-        throw new InvalidAccountIdException(account);
+        return accountManager.updateAccount(account);
     }
 
     @DeleteMapping(path="/int/money/accounts")
     public @ResponseBody List<AccountDTO> deleteAccount(@RequestBody AccountDTO account) throws InvalidAccountIdException {
         LOG.info("Delete account {}", account.getId());
-
-        // Is there an account with this ID?
-        Optional<Account> existingAccount = accountRepository.findById(account.getId());
-        if(existingAccount.isPresent()) {
-            accountRepository.delete(existingAccount.get());
-
-            return getAccounts();
-        }
-
-        throw new InvalidAccountIdException(account);
+        return accountManager.deleteAccount(account);
     }
 }
---
+++ src/main/java/com/jbr/middletier/money/manager/AccountManager.java
@@ -0,0 +1,79 @@
+package com.jbr.middletier.money.manager;
+
+import com.jbr.middletier.money.data.Account;
+import com.jbr.middletier.money.dataaccess.AccountRepository;
+import com.jbr.middletier.money.dto.AccountDTO;
+import com.jbr.middletier.money.exceptions.AccountAlreadyExistsException;
+import com.jbr.middletier.money.exceptions.InvalidAccountIdException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class AccountManager {
+    private final ModelMapper modelMapper;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountManager(ModelMapper modelMapper, AccountRepository accountRepository) {
+        this.modelMapper = modelMapper;
+        this.accountRepository = accountRepository;
+    }
+
+    public List<AccountDTO> getAccounts() {
+        List<AccountDTO> result = new ArrayList<>();
+        for(Account nextAccount: accountRepository.findAll()) {
+            result.add(this.modelMapper.map(nextAccount, AccountDTO.class));
+        }
+
+        Collections.sort(result);
+
+        return result;
+    }
+
+    public List<AccountDTO> createAccount(AccountDTO account) throws AccountAlreadyExistsException {
+        // Is there an account with this ID?
+        Optional<Account> existingAccount = accountRepository.findById(account.getId());
+        if(existingAccount.isPresent()) {
+            throw new AccountAlreadyExistsException(account);
+        }
+
+        accountRepository.save(this.modelMapper.map(account,Account.class));
+
+        return getAccounts();
+    }
+
+    public List<AccountDTO> updateAccount(AccountDTO account) throws InvalidAccountIdException {
+
+        // Is there an account with this ID?
+        Optional<Account> existingAccount = accountRepository.findById(account.getId());
+        if(existingAccount.isPresent()) {
+            existingAccount.get().setColour(account.getColour());
+            existingAccount.get().setImagePrefix(account.getImagePrefix());
+            existingAccount.get().setName(account.getName());
+            accountRepository.save(existingAccount.get());
+
+            return getAccounts();
+        }
+
+        throw new InvalidAccountIdException(account);
+    }
+
+    public List<AccountDTO> deleteAccount(AccountDTO account) throws InvalidAccountIdException {
+
+        // Is there an account with this ID?
+        Optional<Account> existingAccount = accountRepository.findById(account.getId());
+        if(existingAccount.isPresent()) {
+            accountRepository.delete(existingAccount.get());
+
+            return getAccounts();
+        }
+
+        throw new InvalidAccountIdException(account);
+    }
+}
