09467c7175b96f235053f4f55dcf150afe793143
jason <jason@jbrmmg.me.uk>
2022-11-01 18:14:22 +0000

    JBR-339 refactor money

--- src/test/java/com/jbr/middletier/money/ReconciliationTest.java
+++ src/test/java/com/jbr/middletier/money/ReconciliationTest.java
@@ -1,11 +1,18 @@
 package com.jbr.middletier.money;
 
 import com.jbr.middletier.MiddleTier;
+import com.jbr.middletier.money.data.Account;
+import com.jbr.middletier.money.data.Category;
+import com.jbr.middletier.money.data.Statement;
+import com.jbr.middletier.money.data.Transaction;
 import com.jbr.middletier.money.dataaccess.ReconciliationRepository;
+import com.jbr.middletier.money.dataaccess.StatementRepository;
 import com.jbr.middletier.money.dataaccess.TransactionRepository;
 import com.jbr.middletier.money.dto.ReconcileUpdateDTO;
 import com.jbr.middletier.money.dto.ReconciliationFileDTO;
 import com.jbr.middletier.money.exceptions.InvalidAccountIdException;
+import com.jbr.middletier.money.exceptions.InvalidTransactionIdException;
+import com.jbr.middletier.money.exceptions.MultipleUnlockedStatementException;
 import com.jbr.middletier.money.manager.ReconciliationFileManager;
 import com.jbr.middletier.money.manager.ReconciliationManager;
 import org.junit.Assert;
@@ -18,7 +25,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -39,6 +48,8 @@ public class ReconciliationTest extends Support {
     private ReconciliationManager reconciliationManager;
     @Autowired
     private ReconciliationFileManager reconciliationFileManager;
+    @Autowired
+    private StatementRepository statementRepository;
 
     @Before
     public void cleanUp() {
@@ -229,5 +240,96 @@ public class ReconciliationTest extends Support {
                 Assert.assertEquals("HSE", r.getCategory().getId());
             }
         });
+    }
+
+    @Test
+    public void testSetCategoryUpdateInvalidCategory() throws IOException {
+        // load the file.
+        ReconciliationFileDTO reconciliationFile = getReconcileFile();
+        this.reconciliationManager.loadFile(reconciliationFile, reconciliationFileManager);
+
+        final int[] id = {0};
+        this.reconciliationRepository.findAll().forEach(r -> {
+            if(r.getDescription().equals("3CPAYMENT*PRET A MANGER LONDON X")) {
+                id[0] = r.getId();
+            }
+        });
+
+        // Set the category
+        ReconcileUpdateDTO reconcileUpdate = new ReconcileUpdateDTO();
+        reconcileUpdate.setId(id[0]);
+        reconcileUpdate.setType("rec");
+        reconcileUpdate.setCategoryId("XXXX");
+        this.reconciliationManager.processReconcileUpdate(reconcileUpdate);
+
+        this.reconciliationRepository.findAll().forEach(r -> {
+            if(r.getDescription().equals("3CPAYMENT*PRET A MANGER LONDON X")) {
+                Assert.assertNull(r.getCategory());
+            }
+        });
+    }
+
+    @Test
+    public void reconcileInvalidId() throws IOException, MultipleUnlockedStatementException {
+        // load the file.
+        ReconciliationFileDTO reconciliationFile = getReconcileFile();
+        this.reconciliationManager.loadFile(reconciliationFile, reconciliationFileManager);
+
+        try {
+            // There should be no transactions
+            this.reconciliationManager.reconcile(20, true);
+            Assert.fail();
+        } catch (InvalidTransactionIdException ex) {
+            Assert.assertEquals("Cannot find transaction with id 20", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testMultipleUnlockedException() throws IOException, InvalidTransactionIdException {
+        // load the file.
+        ReconciliationFileDTO reconciliationFile = getReconcileFile();
+        this.reconciliationManager.loadFile(reconciliationFile, reconciliationFileManager);
+
+        // Create a second unlocked statement.
+        Statement duplicate = new Statement();
+        AtomicReference<Statement> unlocked = new AtomicReference<>();
+        this.statementRepository.findAll().forEach(s -> {
+            if(s.getId().getAccount().getId().equalsIgnoreCase("AMEX") && !s.getLocked()) {
+               unlocked.set(s);
+            }
+        });
+        duplicate.setOpenBalance(0);
+        duplicate.setLocked(false);
+        Assert.assertNotNull(unlocked.get());
+        duplicate.setId(unlocked.get().getId());
+        duplicate.getId().setMonth(unlocked.get().getId().getMonth()+1);
+        if(duplicate.getId().getMonth() > 12) {
+            duplicate.getId().setMonth(1);
+            duplicate.getId().setYear(unlocked.get().getId().getYear()+1);
+        }
+        this.statementRepository.save(duplicate);
+
+        Account account = new Account();
+        account.setId("AMEX");
+
+        Category category = new Category();
+        category.setId("HSE");
+
+        Transaction testTransaction = new Transaction();
+        testTransaction.setAccount(account);
+        testTransaction.setCategory(category);
+        testTransaction.setAmount(10);
+        testTransaction.setDate(LocalDate.of(2010,5,1));
+        this.transactionRepository.save(testTransaction);
+
+        try {
+            // There should be no transactions
+            this.reconciliationManager.reconcile(testTransaction.getId(), true);
+            Assert.fail();
+        } catch (MultipleUnlockedStatementException ex) {
+            Assert.assertEquals("There are multiple unlocked statements on AMEX", ex.getMessage());
+        }
+
+        this.statementRepository.delete(duplicate);
     }
 }
