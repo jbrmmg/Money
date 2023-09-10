cebfc0170cdd2c685a21dd1ca13f870bd52cba07
jason <jason@jbrmmg.me.uk>
2023-09-10 14:22:06 +0100

    JBR-383 - refactor + upgrade

---
+++ src/main/java/com/jbr/middletier/money/manager/EnvironmentPropertiesPrinter.java
@@ -0,0 +1,22 @@
+package com.jbr.middletier.money.manager;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvironmentPropertiesPrinter {
+    private static final Logger LOG = LoggerFactory.getLogger(EnvironmentPropertiesPrinter.class);
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void logApplicationProperties() {
+        LOG.info("{}={}", "spring.datasource.url", env.getProperty("spring.datasource.url"));
+        LOG.info("{}={}", "spring.datasource.username", env.getProperty("spring.datasource.username"));
+    }
+}
