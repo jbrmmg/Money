150a3725a9016f6c51a03a9cad5361ff1540cc3f
jason <jason@jbrmmg.me.uk>
2024-01-20 13:00:03 +0000

    Tidy up warnings.

--- src/main/java/com/jbr/middletier/money/dto/ReconciliationFileDTO.java
+++ src/main/java/com/jbr/middletier/money/dto/ReconciliationFileDTO.java
@@ -1,7 +1,13 @@
 package com.jbr.middletier.money.dto;
 
+import java.time.LocalDateTime;
+
 public class ReconciliationFileDTO {
     private String filename;
+    private AccountDTO account;
+    private LocalDateTime lastModified;
+    private Long size;
+    private String error;
 
     public String getFilename() {
         return filename;
@@ -9,5 +15,37 @@ public class ReconciliationFileDTO {
 
     public void setFilename(String filename) {
         this.filename = filename;
+    }
+
+    public AccountDTO getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountDTO account) {
+        this.account = account;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(LocalDateTime lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 }
