3ebe4399eb542ce1d928c9ffb83ada1d95a67ee3
jason <jason@jbrmmg.me.uk>
2024-08-26 20:11:10 +0100

    JBR-421 & JBR-407 sonar updates.

--- src/main/java/com/jbr/middletier/money/data/primary/repository/TransactionSpecifications.java
+++ src/main/java/com/jbr/middletier/money/data/primary/repository/TransactionSpecifications.java
@@ -1,72 +1,26 @@
 package com.jbr.middletier.money.data.primary.repository;
 
 import com.jbr.middletier.money.data.primary.Account;
-import com.jbr.middletier.money.data.primary.Category;
 import com.jbr.middletier.money.data.primary.Transaction;
-import com.jbr.middletier.money.util.DateRange;
 import org.springframework.data.jpa.domain.Specification;
 
 import jakarta.persistence.criteria.JoinType;
-import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
-import java.time.LocalDate;
-import java.util.List;
 
 public class TransactionSpecifications {
     private static final String STATEMENT = "statement";
     private static final String ACCOUNT = "account";
-    private static final String CATEGORY = "category";
     private static final String ID = "id";
     private static final String YEAR = "year";
     private static final String MONTH = "month";
-    private static final String DATE = "date";
     private static final String LOCKED = "locked";
 
     private TransactionSpecifications() {
         // Prevent implicit public constructor
     }
 
-    public static Specification<Transaction> accountIn(List<Account> account) {
-        // Account id in a list of values.
-        return (root, criteriaQuery, criteriaBuilder) -> {
-            final Path<String> accountList = root.get(ACCOUNT);
-            return accountList.in(account);
-        };
-    }
-
-    public static Specification<Transaction> categoryIn(List<Category> category) {
-        // Category id in a list of values.
-        return (root, criteriaQuery, criteriaBuilder) -> {
-            final Path<String> categoryList = root.get(CATEGORY);
-            return categoryList.in(category);
-        };
-    }
-
     public static Specification<Transaction> accountIs(Account account) {
         return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get(ACCOUNT), account);
-    }
-
-    public static Specification<Transaction> statementDate(LocalDate statementDate) {
-        // Get the statement id
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.and(
-                criteriaBuilder.equal(root.get(STATEMENT).get(ID).get(YEAR),statementDate.getYear()),
-                criteriaBuilder.equal(root.get(STATEMENT).get(ID).get(MONTH),statementDate.getMonthValue()) );
-    }
-
-    public static Specification<Transaction> statementIsNull() {
-        // Statement is null
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.and(
-                                                            criteriaBuilder.isNull(root.get(STATEMENT).get(ID).get(YEAR)),
-                                                            criteriaBuilder.isNull(root.get(STATEMENT).get(ID).get(MONTH)) );
-    }
-
-    public static Specification<Transaction> datesBetween(DateRange dateRange) {
-        // Strings are dates - from to
-        return (root, criteriaQuery, criteriaBuilder) -> {
-
-            Path<LocalDate> dateEntryPath = root.get(DATE);
-            return criteriaBuilder.between(dateEntryPath,dateRange.getFrom(),dateRange.getTo());
-        };
     }
 
     public static Specification<Transaction> notLocked() {
